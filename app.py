4ef03395c29014af90c9a23979523984df49c31b
svidhi08 <svidhi0811@gmail.com>
2026-03-04 23:06:51 +0530

    modified app and requirement also added Procfile for deployment

--- app.py
+++ app.py
@@ -246,5 +246,5 @@ def essay_grading():
     )
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=False)
